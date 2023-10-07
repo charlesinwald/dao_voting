09ddd65a287d0056ac92601861c9b9e8c96c4386
Charles Inwald <cinwald@gmail.com>
2023-10-07 15:50:04 -0400

    UI enhancements, File upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useSDK } from '@metamask/sdk-react';
@@ -8,7 +8,31 @@ function App() {
   const [account, setAccount] = useState<string>();
   const [joined, setJoined] = useState<boolean>(false);
   const [svgData, setSvgData] = useState<string>("");
+  const [invalidSvg, setInvalidSvg] = useState<boolean>(false);
+
   const { sdk, connected, connecting, provider, chainId } = useSDK();
+
+  const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file && file.type === "image/svg+xml") {
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        setSvgData(event.target?.result as string);
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  const validateAndSetSvg = (data: string) => {
+    // Here you can put logic to validate the SVG data
+    // For now, let's just check if it starts with '<svg'
+    if (data.trim().startsWith('<svg')) {
+      setInvalidSvg(false);
+      setSvgData(data);
+    } else {
+      setInvalidSvg(true);
+    }
+  };
 
   const connect = async () => {
     try {
@@ -47,6 +71,9 @@ function App() {
       });
       const data = await res.json();
       console.log("Transaction Hash:", data.transactionHash);
+      if (data.transactionHash) {
+        setSvgData("");
+      }
     } catch (error) {
       console.error("Error:", error);
     }
@@ -59,25 +86,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div
-          className="App-link"
-        >
+        <div className="App-link">
           {!account ?
-            <button onClick={connect} disabled={connecting}>Connect with MetaMask</button> :
-            <h3>{`Connected Account: ${account}`}</h3>
+            <button className="connect-button" onClick={connect} disabled={connecting}>Connect with MetaMask</button> :
+            <p className="connected-account">{`Connected Account: ${account}`}</p>
           }
-          {account && !joined && <button onClick={joinDAO}>Join DAO</button>}
-          {joined && <p>Joined DAO!</p>}
+          {account && !joined && <button className="join-button" onClick={joinDAO}>Join DAO</button>}
+          {joined && <p className="joined-message">Joined DAO!</p>}
         </div>
-        {joined && <div>
+        {joined && <div className="proposal-section">
           <h3>Create a Proposal</h3>
           <textarea
-            placeholder="Enter your SVG here"
+            className="svg-textarea"
+            placeholder="Paste your SVG here"
             value={svgData}
-            onChange={(e) => setSvgData(e.target.value)}
+            onChange={(e) => validateAndSetSvg(e.target.value)}
           />
-          <button onClick={createProposal}>Submit Proposal</button>
+          {svgData && <button className="clear-button" onClick={() => setSvgData("")}>Clear</button>}
+          <button className="submit-button" onClick={createProposal}>Submit Proposal</button>
+          {invalidSvg && <p className="invalid-svg">Invalid SVG!</p>}
+          <input type="file" className="file-input" accept="image/svg+xml" onChange={handleFileUpload} />
+          {svgData && <div className="preview-section">
+            {!invalidSvg && <p>SVG Preview</p>}
+            {!invalidSvg && <div dangerouslySetInnerHTML={{ __html: svgData }} />}
+          </div>}
         </div>}
       </header>
     </div>
